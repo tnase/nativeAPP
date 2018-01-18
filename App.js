dad6769aae564592abda73311409e50d5632262a
Alain tona <alaintona1@gmail.com>
2018-01-18 10:50:04 +0100

    second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>moi je regarde ma mere marcher  .</Text>
+        <Text>moi je regarde ma mere marcher hefehffiufheoiuhe .</Text>
       </View>
     );
   }
