9c8b06f4771e2fcaa5bdd44083cc9b02eff58f97
Alain tona <alaintona1@gmail.com>
2018-01-18 10:38:44 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>test echo alors c'est votre pb sa nuhuiehfehfeuefhue me regarde point quand tu sera malade tu vera .</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
